5fe20cd2dd8bd192d6e50a434d05ef262a361f51
huanghfzhufeng <2461646599@qq.com>
2026-01-03 00:34:56 +0800

    支持部署

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages 部署时，改成你的仓库名，如 '/nano-banana-watermark-remover/'
+  // 本地开发或其他平台部署时用 '/'
+  base: '/',
 })
